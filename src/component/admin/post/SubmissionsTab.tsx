93c9563b1a7d6be321844f37f180a6ca05bc4aca
PhamSyNhatNhan <phamsynhatnhan35@gmail.com>
2025-09-18 02:45:22 +0700

    mentor update 39

--- src/component/admin/post/SubmissionsTab.tsx
+++ src/component/admin/post/SubmissionsTab.tsx
@@ -255,7 +255,7 @@ const SubmissionsTab = React.forwardRef<{ reload: () => void }, SubmissionsTabPr
 
             console.log('Final submissions with tags:', filteredSubmissions);
 
-            setSubmissions(filteredSubmissions);
+            setSubmissions(filteredSubmissions as PostSubmission[]);
             setTotalCount(count || 0);
             setTotalPages(Math.ceil((count || 0) / pageSize));
 
