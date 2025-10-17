5bcd7565541b4e8d93ff0f61519b187e5056658e
PhamSyNhatNhan <phamsynhatnhan35@gmail.com>
2025-10-18 00:25:52 +0700

    admin update 7

--- src/component/admin/mentor_booking/EventsTab.tsx
+++ src/component/admin/mentor_booking/EventsTab.tsx
@@ -2408,7 +2408,7 @@ export const EventsTab: React.FC<EventsTabProps> = ({
                                             const svgData = new XMLSerializer().serializeToString(svg);
                                             const canvas = document.createElement('canvas');
                                             const ctx = canvas.getContext('2d');
-                                            const img = new Image();
+                                            const img = new window.Image();
 
                                             img.onload = () => {
                                                 canvas.width = img.width;
